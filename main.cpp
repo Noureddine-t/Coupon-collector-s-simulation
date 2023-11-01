260273770eab0f66906dbfe5a130d3e1209779ea
Nour_Eddine-T <noureddinetaleb.mail@gmail.com>
2023-11-01 19:19:00 +0100

    possiblite de modifier nombre de vignette trouve

--- main.cpp
+++ main.cpp
@@ -9,32 +9,44 @@ int simulateWithMultipleCollectionsWithExchange(int, int);
 
 int main() {
     int collectionNumber = 100; //valeur par defaut pour nombre total de collection
-
+    int simulations = 1000;  // Nombre de simulations à exécuter pour avoir une valeur moyenne plus exacte
+    int vignetteNumber = 5; //nombre de vignette dans le céréale pour la 3eme fonction
     while (true) {
         //demander nombre de collection
-        std::cout << "Entrez nombre de vignettes de votre collection (0 pour arreter programme) : "<<std::endl;
+        std::cout << "Entrez nombre de vignettes de votre collection (0 pour arreter programme)  " << std::endl;
+        std::cout << "Ici : ";
         std::cin >> collectionNumber;
 
         //condition d'arret
         if (collectionNumber == 0)
             break;
 
-        int simulations = 1000;  // Nombre de simulations à exécuter pour avoir une valeur moyenne plus exacte
         //demander nombre de simulations
-        std::cout << "Entrez nombre de simulations (0 pour arreter programme) : "<<std::endl;
+        std::cout << "Entrez nombre de simulations (0 pour arreter programme)  " << std::endl;
+        std::cout << "Ici : ";
         std::cin >> simulations;
-        std::cout << std::endl;
 
         //condition d'arret
         if (simulations == 0)
             break;
+
+        //demander nombre de vignette dans le céréale pour la fonction 3
+        std::cout << "Entrez nombre de vignettes (0 pour arreter programme)  " << std::endl;
+        std::cout << "Ici : ";
+        std::cin >> vignetteNumber;
+        std::cout << std::endl;
+
+        //condition d'arret
+        if (vignetteNumber == 0)
+            break;
+
 
         double averageWeeks[] = {0, 0, 0};//semaines necessaires avec echange et sans echange
         //calcule moyenne de semaines
         for (int i = 0; i < simulations; i++) {
             averageWeeks[0] += simulateCollection(collectionNumber);
             averageWeeks[1] += simulateCollectionWithExchange(collectionNumber);
-            averageWeeks[2] += simulateWithMultipleCollectionsWithExchange(collectionNumber, 5);
+            averageWeeks[2] += simulateWithMultipleCollectionsWithExchange(collectionNumber, vignetteNumber);
         }
         averageWeeks[0] /= simulations;
         averageWeeks[1] /= simulations;
@@ -45,7 +57,8 @@ int main() {
         std::cout << "Il faut en moyenne " << averageWeeks[1] << " semaines pour completer la collection de "
                   << collectionNumber << " vignettes avec echange 10 contre 1." << std::endl;
         std::cout << "Il faut en moyenne " << averageWeeks[2] << " semaines pour completer la collection de "
-                  << collectionNumber << " vignettes avec echange et vignette multiple." << std::endl;
+                  << collectionNumber << " vignettes avec echange et " << vignetteNumber << " dans le cereale"
+                  << std::endl;
     }
     return 0;
 }
@@ -70,7 +83,7 @@ int simulateCollection(int collectionNumber) {
         //verifier si tout les vignettes sont collectées pour finir
         allCollected = true;
         for (int i = 0; i < collectionNumber; i++) {
-            if (!collected[i]){
+            if (!collected[i]) {
                 allCollected = false;
                 break;
             }
@@ -124,7 +137,7 @@ int simulateCollectionWithExchange(int collectionNumber) {
     return weeks;
 }
 
-int simulateWithMultipleCollectionsWithExchange(int collectionNumber, int VignetteNumber) {
+int simulateWithMultipleCollectionsWithExchange(int collectionNumber, int vignetteNumber) {
 
     bool *collected = new bool[collectionNumber]; // Allocation dynamique du tableau
 
@@ -140,10 +153,10 @@ int simulateWithMultipleCollectionsWithExchange(int collectionNumber, int Vignet
         //std::cout << "semaine:" << weeks << std::endl;
 
         //for (int i = 0; i < collectionNumber; i++)
-         // std::cout << "tableau collected:" << collected[i] << std::endl;
+        // std::cout << "tableau collected:" << collected[i] << std::endl;
 
-        int *newVignetteTable = new int[VignetteNumber]; //si on a plus d'une vignette dans le céréale
-        for (int i = 0; i < VignetteNumber; i++) {
+        int *newVignetteTable = new int[vignetteNumber]; //si on a plus d'une vignette dans le céréale
+        for (int i = 0; i < vignetteNumber; i++) {
             newVignetteTable[i] = rand() % collectionNumber;
             if (!collected[newVignetteTable[i]])
                 collected[newVignetteTable[i]] = true;
