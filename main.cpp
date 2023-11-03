fe34337e14efe755ffefadef19958715a7715274
Nour_Eddine-T <noureddinetaleb.mail@gmail.com>
2023-11-03 18:47:16 +0100

    implementation directe des fonctions theorique

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int simulateWithMultipleCollectionsWithExchange(int, int);
 
 double theoricalValueUsingHarmonicSerie(int);
 
-double theoricalValueUsingAsymptoticDevelopment(int collectionNumber);
+double theoricalValueUsingAsymptoticDevelopment(int);
 
 int main() {
     int collectionNumber = 100; //valeur par defaut pour nombre total de collection
@@ -18,7 +18,7 @@ int main() {
     while (true) {
         //demander nombre de collection
         std::cout << "Entrez nombre de vignettes de votre collection (0 pour arreter programme)  " << std::endl;
-        std::cout << "Ici : ";
+        std::cout << "Ici >  ";
         std::cin >> collectionNumber;
 
         //condition d'arret
@@ -27,7 +27,7 @@ int main() {
 
         //demander nombre de simulations
         std::cout << "Entrez nombre de simulations (0 pour arreter programme)  " << std::endl;
-        std::cout << "Ici : ";
+        std::cout << "Ici >  ";
         std::cin >> simulations;
 
         //condition d'arret
@@ -36,7 +36,7 @@ int main() {
 
         //demander nombre de vignette dans le céréale pour la fonction 3
         std::cout << "Entrez nombre de vignettes (0 pour arreter programme)  " << std::endl;
-        std::cout << "Ici : ";
+        std::cout << "Ici >  ";
         std::cin >> vignetteNumber;
         std::cout << std::endl;
 
@@ -44,7 +44,7 @@ int main() {
         if (vignetteNumber == 0)
             break;
 
-        double averageWeeks[] = {0, 0, 0, 0, 0};//semaines necessaires avec echange et sans echange
+        double averageWeeks[] = {0, 0, 0};//semaines necessaires avec echange et sans echange
 
         //calcule moyenne de semaines
         for (int i = 0; i < simulations; i++) {
@@ -52,27 +52,26 @@ int main() {
             averageWeeks[0] += simulateCollection(collectionNumber);
             averageWeeks[1] += simulateCollectionWithExchange(collectionNumber);
             averageWeeks[2] += simulateWithMultipleCollectionsWithExchange(collectionNumber, vignetteNumber);
-            averageWeeks[3] += theoricalValueUsingHarmonicSerie(collectionNumber);
-            averageWeeks[4] += theoricalValueUsingAsymptoticDevelopment(collectionNumber);
         }
         averageWeeks[0] /= simulations;
         averageWeeks[1] /= simulations;
         averageWeeks[2] /= simulations;
-        averageWeeks[3] /= simulations;
-        averageWeeks[4] /= simulations;
+
         //Affichage
-        std::cout << "Il faut en moyenne " << averageWeeks[0] << " semaines pour completer la collection de "
+        std::cout << "-Simulation :" << std::endl;
+        std::cout << ">> Il faut en moyenne " << averageWeeks[0] << " semaines pour completer la collection de "
                   << collectionNumber << " vignettes sans echange." << std::endl;
-        std::cout << "Il faut en moyenne " << averageWeeks[1] << " semaines pour completer la collection de "
+        std::cout << ">> Il faut en moyenne " << averageWeeks[1] << " semaines pour completer la collection de "
                   << collectionNumber << " vignettes avec echange 10 contre 1." << std::endl;
-        std::cout << "Il faut en moyenne " << averageWeeks[2] << " semaines pour completer la collection de "
+        std::cout << ">> Il faut en moyenne " << averageWeeks[2] << " semaines pour completer la collection de "
                   << collectionNumber << " vignettes avec echange et " << vignetteNumber << " dans le cereale"
-                  << std::endl;
-        std::cout << "Il faut en moyenne " << averageWeeks[3]
+                  << std::endl << std::endl;
+        std::cout << "-Valeur theorique :" << std::endl;
+        std::cout << ">> Il faut en moyenne " << theoricalValueUsingHarmonicSerie(collectionNumber)
                   << " semaines pour completer la collection de "
-                  << collectionNumber << " vignettes sans echange a l'aide de la formule theorique somme 1/k."
+                  << collectionNumber << " vignettes sans echange a l'aide de la formule theorique n*somme(1/k)."
                   << std::endl;
-        std::cout << "Il faut en moyenne " << averageWeeks[4]
+        std::cout << ">> Il faut en moyenne " << theoricalValueUsingAsymptoticDevelopment(collectionNumber)
                   << " semaines pour completer la collection de "
                   << collectionNumber << " vignettes sans echange a l'aide de l'approximation de la forumule theorique."
                   << std::endl;
