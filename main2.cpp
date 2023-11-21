88648eb8a8a843ac035122479abb8c636f96f9e8
Nour_Eddine-T <noureddinetaleb.mail@gmail.com>
2023-11-21 21:19:08 +0100

    correction fonction asymptotique

---
+++ main2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+
+#include "Functions.h"
+
+int main() {
+    int N = 170;
+    int simulations = 1000;
+    int averageWeeks[2];
+
+    // Open the CSV file for writing
+    std::ofstream outputFile("output.csv");
+
+    // Check if the file is opened successfully
+    if (!outputFile.is_open()) {
+        std::cerr << "Error opening output file!" << std::endl;
+        return 1;  // Exit with an error code
+    }
+
+    // Write header to the CSV file
+    outputFile << "N, AverageWeeksWithoutExchange, AverageWeeksWithExchange" << std::endl;
+
+    for (int i = 1; i < N; i++) {
+        std::cout << "N=" << i << std::endl;
+        averageWeeks[0] = 0;
+        averageWeeks[1] = 0;
+        for (int j = 0; j < simulations; j++) {
+            averageWeeks[0] += simulateWithMultipleCollections(50, i);
+            averageWeeks[1] += simulateWithMultipleCollectionsWithExchange(50, i);
+        }
+        averageWeeks[0] /= simulations;
+        averageWeeks[1] /= simulations;
+
+        // Write data to the CSV file
+        outputFile << i << "," << averageWeeks[0] << "," << averageWeeks[1] << std::endl;
+
+        std::cout << averageWeeks[0] << " ";
+        std::cout << averageWeeks[1] << " " << std::endl;
+    }
+
+    // Close the CSV file
+    outputFile.close();
+
+    return 0;
+}
