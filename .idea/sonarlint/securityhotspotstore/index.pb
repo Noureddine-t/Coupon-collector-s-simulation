ef25c19b96a34e2c45805e323cec3ff6e9291312
Nour_Eddine-T <noureddinetaleb.mail@gmail.com>
2023-12-25 05:23:38 +0100

    Ajout du makefile et Functions.cpp

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -15,8 +15,6 @@
 ‚script_r/4_visualiser_le_prix_d_echange_avec_2_vignettes/_SemaineEnFonctionDeNombreA_Collecte_Avec_2_Determiner_taux_d_echange.csv,9\6\965dffd6bb05dea91d1fe2ccf3df0704b21a7809
 É
 ˜script_r/5_nbr_de_vignette_dans_collection_en_fct_de_prix_echange/nombre_paquet_a_acheter_en_fct_de_prix_echange_avec_1_2_3_4_5_vignette_par_cereale.csv,1\2\12aa9b39ea19085e8a5cdbcf94d037051091be1b
-¥
-uscript_r/4_visualiser_le_prix_d_echange_avec_2_vignettes/AverageWeeksVSTotalNumberOfCollection_FindingExchangeValue.R,c\6\c69c0fafb51fb8cd83e073dd632661bb84582f09
 9
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
 Š
@@ -29,8 +27,6 @@ ascript_r/3_AverageWeeksVSTotalNumberOfCollection/_SemainesEnFonctionDeNombreA_C
 ˜script_r/6_CollectionNumber_en_fonction_du_taux_echange/_CollectionNumber_en_fonction_du_taux_echange_pour_croiser_deux_courbe_avec_vignetteNumber_2.csv,e\8\e8ee8136d70c53d03a121b21a228024d94f0623c
 Š
 ZScript_r/3_AverageWeeksVSTotalNumberOfCollection/_SemainesEnFonctionDeNombreA_Collecte.csv,3\5\35610baeac7c46e69024ef513f440232e64ebab3
-¥
-uScript_r/4_visualiser_le_prix_d_echange_avec_2_vignettes/AverageWeeksVSTotalNumberOfCollection_FindingExchangeValue.R,7\a\7a438ab23fcdce868b3cc9631ebdf9fac607457a
 ³
 ‚Script_r/4_visualiser_le_prix_d_echange_avec_2_vignettes/_SemaineEnFonctionDeNombreA_Collecte_Avec_2_Determiner_taux_d_echange.csv,6\d\6d1427091f9216c67197a4d4e2d7929704f119d8
 É
@@ -40,3 +36,7 @@ uScript_r/4_visualiser_le_prix_d_echange_avec_2_vignettes/AverageWeeksVSTotalNum
 :
 
 .gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+=
+Functions.cpp,1\e\1ebfe5af0a2a90279402ecb6a6b50f492d2dc511
+8
+Makefile,8\3\836efb6e25a091dcb4ff8e1dbb2f0be6a5cbf14c
---
+++ Functions.cpp
@@ -0,0 +1,325 @@
+//
+// Created by noure on 25/12/2023.
+//
+#include "Functions.h"
+
+int simulateCollection(int collectionNumber) {
+
+    bool *collected = new bool[collectionNumber]; // Allocation dynamique du tableau
+
+    for (int i = 0; i < collectionNumber; i++) //initialiser tableau de vignettes
+        collected[i] = false;
+
+    int weeks = 0;
+    bool allCollected = false;
+
+    while (!allCollected) {
+        weeks++;
+        int newVignette = rand() % collectionNumber; //choisir un valeur alétoire qui represente une vignette
+        if (!collected[newVignette])
+            collected[newVignette] = true;
+
+        //verifier si tout les vignettes sont collectées pour finir, si la nouvelle vignette est déja présente je verifie pas
+        allCollected = true;
+        for (int i = 0; i < collectionNumber; i++) {
+            if (!collected[i]) {
+                allCollected = false;
+                break;
+            }
+        }
+    }
+    delete[] collected; // Libération de la mémoire allouée dynamiquement
+    return weeks;
+}
+
+int simulateCollectionWithExchange(int collectionNumber) {
+
+    bool *collected = new bool[collectionNumber]; // Allocation dynamique du tableau
+
+    for (int i = 0; i < collectionNumber; i++)
+        collected[i] = false;
+
+    int weeks = 0;
+    int duplicatesVignette = 0;
+    bool allCollected = false;
+    int notCollectedCounter = collectionNumber;
+    while (!allCollected) {
+        weeks++;
+
+        int newVignette = rand() % collectionNumber;
+        if (!collected[newVignette]) {
+            collected[newVignette] = true;
+            //compter vignettes restantes
+            notCollectedCounter--;
+        } else
+            duplicatesVignette++;
+
+
+        //faire echange a la fin (si nombre de vignettes manquantes = doublons/10 on echange) et mettre fin
+        if (notCollectedCounter <= duplicatesVignette / 10) {
+            break;
+        }
+
+
+        //verifier si tout les vignettes collecter pour finir
+        allCollected = true;
+        for (int i = 0; i < collectionNumber; i++) {
+            if (!collected[i]) {
+                allCollected = false;
+                break;
+            }
+        }
+    }
+    delete[] collected; // Libération de la mémoire allouée dynamiquement
+    return weeks;
+}
+
+int simulateWithMultipleCollections(int collectionNumber, int vignetteNumber) {
+
+    bool *collected = new bool[collectionNumber]; // Allocation dynamique du tableau
+
+    for (int i = 0; i < collectionNumber; i++)
+        collected[i] = false;
+
+    int weeks = 0;
+    bool allCollected = false;
+
+    //exception si collectionNumber<vignetteNumber alors weeks = 1
+    if (collectionNumber < vignetteNumber) {
+        return 1;
+    }
+    int *newVignetteTable = new int[vignetteNumber]; // si on a plus d'une vignette dans la boite de céréales
+
+    while (!allCollected) {
+        weeks++;
+
+        for (int i = 0; i < vignetteNumber; i++) {
+            int randomVignette;
+            bool isUnique; //Assurer que les vignettes trouvées dans le céréal soient différentes
+
+            do {
+                // Générer un nombre aléatoire entre 0 et collectionNumber - 1
+                randomVignette = rand() % collectionNumber;
+
+                // Vérifier si le nombre généré est déjà présent dans le tableau
+                isUnique = true;
+                for (int j = 0; j < i; j++) {
+                    if (newVignetteTable[j] == randomVignette) {
+                        isUnique = false;
+                        break;
+                    }
+                }
+            } while (!isUnique);
+
+            // Assigner le nombre aléatoire unique au tableau
+            newVignetteTable[i] = randomVignette;
+
+            // Marquer la vignette comme collectée
+            if (!collected[newVignetteTable[i]])
+                collected[newVignetteTable[i]] = true;
+        }
+
+        //verifier si toutes les vignettes sont collectées
+        allCollected = true;
+        for (int i = 0; i < collectionNumber; i++) {
+            if (!collected[i]) {
+                allCollected = false;
+                break;
+            }
+        }
+    }
+    delete[] newVignetteTable;
+    delete[] collected; // Libération de la mémoire allouée dynamiquement
+    return weeks;
+}
+
+int simulateWithMultipleCollectionsWithExchange(int collectionNumber, int vignetteNumber) {
+
+    bool *collected = new bool[collectionNumber]; // Allocation dynamique du tableau
+
+    for (int i = 0; i < collectionNumber; i++)
+        collected[i] = false;
+
+    int weeks = 0;
+    int duplicatesVignette = 0;
+    bool allCollected = false;
+    int notCollectedCounter = collectionNumber;
+
+    if (collectionNumber < vignetteNumber) {
+        return 1;
+    }
+    int *newVignetteTable = new int[vignetteNumber]; //si on a plus d'une vignette dans la boite de céréales
+
+    while (!allCollected) {
+        weeks++;
+
+        for (int i = 0; i < vignetteNumber; i++) {
+            int randomVignette;
+            bool isUnique; //Assurer que les vignettes trouvées dans le céréal soient différentes
+
+            do {
+                // Générer un nombre aléatoire entre 0 et collectionNumber - 1
+                randomVignette = rand() % collectionNumber;
+
+                // Vérifier si le nombre généré est déjà présent dans le tableau
+                isUnique = true;
+                for (int j = 0; j < i; j++) {
+                    if (newVignetteTable[j] == randomVignette) {
+                        isUnique = false;
+                        break;
+                    }
+                }
+            } while (!isUnique);
+
+            // Assigner le nombre aléatoire unique au tableau
+            newVignetteTable[i] = randomVignette;
+
+            // Marquer la vignette comme collectée
+            if (!collected[newVignetteTable[i]]) {
+                collected[newVignetteTable[i]] = true;
+                notCollectedCounter--;
+            } else
+                duplicatesVignette++;
+
+        }
+        if (notCollectedCounter <= duplicatesVignette / 10) {
+            break;
+        }
+
+        //verifier si toutes les vignettes sont collectées
+        allCollected = true;
+        for (int i = 0; i < collectionNumber; i++) {
+            if (!collected[i]) {
+                allCollected = false;
+                break;
+            }
+        }
+    }
+    delete[] newVignetteTable;
+    delete[] collected; // Libération de la mémoire allouée dynamiquement
+
+    return weeks;
+}
+
+//on utilisant série harmonique: N*sum(1/k) de 1 a N
+double theoricalValueUsingHarmonicSerie(int collectionNumber) {
+    double somme = 0;
+    for (int i = 1; i <= collectionNumber; i++)
+        somme += 1.0 / i;
+    return collectionNumber * somme;
+}
+
+//on utilisant En utilisant le développement asymptotique E(Tn)=n⋅Hn=nln(n)+γ⋅n+1/2+o(1) où γ≈0.5772156649 est la constante d'Euler-Mascheroni.
+double theoricalValueUsingAsymptoticDevelopment(int collectionNumber) {
+    return collectionNumber * (std::log(collectionNumber) + 0.57721) + 1. / 2;  // Utilise ln(n) + γ;
+}
+
+int simulateCollectionWithExchange(int collectionNumber, int Exchange) {
+
+    bool *collected = new bool[collectionNumber]; // Allocation dynamique du tableau
+
+    for (int i = 0; i < collectionNumber; i++)
+        collected[i] = false;
+
+    int weeks = 0;
+    int duplicatesVignette = 0;
+    bool allCollected = false;
+    int notCollectedCounter = collectionNumber;
+    while (!allCollected) {
+        weeks++;
+
+        int newVignette = rand() % collectionNumber;
+        if (!collected[newVignette]) {
+            collected[newVignette] = true;
+            //compter vignettes restantes
+            notCollectedCounter--;
+        } else
+            duplicatesVignette++;
+
+
+        //faire echange a la fin (si nombre de vignettes manquantes = doublons/10 on echange) et mettre fin
+        if (notCollectedCounter <= duplicatesVignette / Exchange) {
+            break;
+        }
+
+
+        //verifier si tout les vignettes collecter pour finir
+        allCollected = true;
+        for (int i = 0; i < collectionNumber; i++) {
+            if (!collected[i]) {
+                allCollected = false;
+                break;
+            }
+        }
+    }
+    delete[] collected; // Libération de la mémoire allouée dynamiquement
+    return weeks;
+}
+
+int simulateWithMultipleCollectionsWithExchange(int collectionNumber, int vignetteNumber, int exchange) {
+
+    bool *collected = new bool[collectionNumber]; // Allocation dynamique du tableau
+
+    for (int i = 0; i < collectionNumber; i++)
+        collected[i] = false;
+
+    int weeks = 0;
+    int duplicatesVignette = 0;
+    bool allCollected = false;
+    int notCollectedCounter = collectionNumber;
+
+    if (collectionNumber < vignetteNumber) {
+        return 1;
+    }
+    int *newVignetteTable = new int[vignetteNumber]; //si on a plus d'une vignette dans la boite de céréales
+
+    while (!allCollected) {
+        weeks++;
+
+        for (int i = 0; i < vignetteNumber; i++) {
+            int randomVignette;
+            bool isUnique; //Assurer que les vignettes trouvées dans le céréal soient différentes
+
+            do {
+                // Générer un nombre aléatoire entre 0 et collectionNumber - 1
+                randomVignette = rand() % collectionNumber;
+
+                // Vérifier si le nombre généré est déjà présent dans le tableau
+                isUnique = true;
+                for (int j = 0; j < i; j++) {
+                    if (newVignetteTable[j] == randomVignette) {
+                        isUnique = false;
+                        break;
+                    }
+                }
+            } while (!isUnique);
+
+            // Assigner le nombre aléatoire unique au tableau
+            newVignetteTable[i] = randomVignette;
+
+            // Marquer la vignette comme collectée
+            if (!collected[newVignetteTable[i]]) {
+                collected[newVignetteTable[i]] = true;
+                notCollectedCounter--;
+            } else
+                duplicatesVignette++;
+
+        }
+        if (notCollectedCounter <= duplicatesVignette / exchange) {
+            break;
+        }
+
+        //verifier si toutes les vignettes sont collectées
+        allCollected = true;
+        for (int i = 0; i < collectionNumber; i++) {
+            if (!collected[i]) {
+                allCollected = false;
+                break;
+            }
+        }
+    }
+    delete[] newVignetteTable;
+    delete[] collected; // Libération de la mémoire allouée dynamiquement
+
+    return weeks;
+}
